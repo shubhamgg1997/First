e05423b0ef869d278f60a3f52574cd28db4a41de
shubham gupta <Aman.Gupa-ext@ab-Inbev.com>
2021-01-18 21:54:46 +0000

    First Commit

---
+++ Basic150.py
@@ -0,0 +1,41 @@
+# https://pynative.com/python-basic-exercise-for-beginners/
+#15 Questions
+# x = int(input("Please provide two digit number: "))
+# y = int(input("Please provide two digit number: "))
+
+
+# def number(x,y):
+        # if x*y > 1000:
+            # print (x+y)
+        # else:
+            # print(x*y)
+
+
+
+# number(x,y)
+
+# x=range(10)
+
+# y=0
+# for i in x:
+    # sum = y+i
+    # print( "Current Number", i, "Previous Number ", y," Sum: ", sum)
+    # y=i
+
+# x="Sring"
+
+# for i in range(0,len(x)-1,2):
+      # print(x[i])
+
+
+
+
+# str="pynative" 
+# # for i in range(0,len(str)-1,2):
+        # # print("index[",i,"]", str[i] )
+        
+x=("Emma is good developer. Emma is a writer")
+
+for i in x:
+        if i =="Emma":
+            print(i)
